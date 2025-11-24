e3113a5a2133d22207fbf0f592270d747033aad1
FionnSlattery <fionnslattery60@gmail.com>
2025-11-24 19:47:00 +0000

    Initial Spring Boot Project

---
+++ src/main/java/com/example/Fionnspetitions/FionnspetitionsApplication.java
@@ -0,0 +1,13 @@
+package com.example.Fionnspetitions;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FionnspetitionsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FionnspetitionsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/Fionnspetitions/FionnspetitionsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Fionnspetitions;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FionnspetitionsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
